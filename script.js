4d0d654858a8ce190551eeae3b1edb55da5afa6c
Vlad <example@gmail.com>
2020-04-01 13:49:47 +0200

    add timer

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
+    
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -29,9 +30,56 @@ window.addEventListener('DOMContentLoaded', function() {
                     hideTabContent(0);
                     showTabContent(i);
                     break;
+
                 }
             }
         }
 
     });
+
+
+
+    //timer 
+
+    let deadline = '2020-04-08';
+
+    function getTimeRemaining(endtime){
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));     
+        // hours = Math.floor((t/1000/60/60) % 24) ; 
+        //для дней       
+        //days =  Math.floor((t/1000*60*60*24)); 
+
+        return{
+            'total' : t,
+            'hours' : hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime){
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+            timeInterval = setInterval(updateClock, 1000);
+       
+    function updateClock(){
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if(t.total <= 0){
+                clearInterval(timeInterval);
+            }
+        }    
+    }
+
+    setClock('timer', deadline);
+
+
 });
