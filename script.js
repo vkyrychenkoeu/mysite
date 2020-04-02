ccc4c0ae1fe3b94bda090d3a5afeccd8bb3f1226
Vlad <example@gmail.com>
2020-04-02 22:01:03 +0200

    add modalnoje okno

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ window.addEventListener('DOMContentLoaded', function() {
         let timer = document.getElementById(id),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
-            seconds = timer.querySelector('.seconds');
+            seconds = timer.querySelector('.seconds'),
             timeInterval = setInterval(updateClock, 1000);
        
     function updateClock(){
@@ -81,5 +81,23 @@ window.addEventListener('DOMContentLoaded', function() {
 
     setClock('timer', deadline);
 
+
+    //Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function(){
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
 
 });
