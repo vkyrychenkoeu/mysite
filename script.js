5cec0afdb40c6dfda0743e707c1f430de742ceb0
Vlad <example@gmail.com>
2020-05-04 15:57:55 +0200

    add Slider

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
     //timer 
 
-    let deadline = '2020-04-08';
+    let deadline = '2020-05-30';
 
     function getTimeRemaining(endtime){
         let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -121,8 +121,114 @@ window.addEventListener('DOMContentLoaded', function() {
     
     // item.createDiv();
 
+//Form
+   
+let message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы свами свяжемся!)',
+    failure: 'Что-то пошло не так...'
+};
+
+let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    form.appendChild(statusMessage);
+
+    let request = new XMLHttpRequest();
+    request.open('POST', 'server.php');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+    let formData = new FormData(form);
+
+    let obj = {};
+    formData.forEach(function(value, key) {
+        obj[key] = value;
+    });
+    let json = JSON.stringify(obj);
+
+    request.send(json);
+
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState < 4) {
+            statusMessage.innerHTML = message.loading;
+        } else if(request.readyState === 4 && request.status == 200) {
+            statusMessage.innerHTML = message.success;
+        } else {
+            statusMessage.innerHTML = message.failure;
+        }
+    });
+
+    for (let i = 0; i < input.length; i++) {
+        input[i].value = '';
+    }
+});
 
 
+    //Slider
 
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot'); 
+
+    showSlides(slideIndex);
+
+    function showSlides(n){
+
+        if(n > slides.length){
+            slideIndex = 1;
+        }
+        if (n < 1){
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        //тоже самое но обычный цикл, с верху представлен более современный
+        // и удобный вариант такой же функции
+        // for (let i = 0; i < slides.length; i++){
+        //     slides[i].style.display = 'none';
+        // }
+
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }     
+
+
+    //что бы функция понимала что надо поменять слайд 
+    //пишем функцию для длбавления н + 1
+    function plusSlides(n){
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n){
+        showSlides(slideIndex = n);
+    }
+
+
+    //перемещение в слайдере вперед/назад 
+    prev.addEventListener('click', function(){
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function(){
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event){
+        for ( let i = 0; i < dots.length + 1; i++){
+            if(event.target.classList.contains('dot') && event.target == dots[i-1]){
+                currentSlide(i);
+            }
+        }
+    });
 
 });
